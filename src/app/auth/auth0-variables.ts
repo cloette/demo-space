0fd0c54241e4b3168b5236b4203e3205367baf95
Cloette <laurenowensby_2016@depauw.edu>
2018-03-23 19:36:21 -0400

    ooops forgot https

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -7,5 +7,5 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: '11Lc1USJZ9v7S5Jib28bbd7j2TiMcgiD',
   domain: 'app90438451.auth0.com',
-  callbackURL: 'http://objective-assessor-dev.herokuapp.com/callback'
+  callbackURL: 'https://objective-assessor-dev.herokuapp.com/callback'
 };
