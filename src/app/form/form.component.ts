5f9c63392a3051a47e893508c3c7c0fabb898583
Cloette <clozager@gmail.com>
2018-07-01 19:34:20 -0400

    Don't put a toggle inside of a !toggle

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -31,7 +31,7 @@ export class FormComponent implements OnInit {
   public dataReady: boolean = false;
   public formID: string = undefined;
   public form: IFormResponse;
-  public fields: Array<IFieldResponse>;
+  public fields;
   public profile;
   public newField: IFieldResponse;
 
