b594df9a400f9395639f05633fd3dc3661572eea
Cloette <clozager@gmail.com>
2018-08-26 14:04:18 -0400

    :( still don't know why home isn't working

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -4,10 +4,9 @@ import { CallbackComponent } from './callback/callback.component';
 
 export const routes: Route[] = [
   { path: '', pathMatch: 'full', component: HomeComponent},
-  { path: 'home', component: HomeComponent},
   { path: 'callback', pathMatch: 'full', component: CallbackComponent },
   { loadChildren: 'app/form/form.module#FormModule', path: 'form' },
   { loadChildren: 'app/item/item.module#ItemModule', path: 'item' },
   { loadChildren: 'app/leaderboard/leaderboard.module#LeaderboardModule', path: 'leaderboard' },
-  { path: '**', redirectTo: 'home' }
+  { path: '**', redirectTo: '' }
 ];
